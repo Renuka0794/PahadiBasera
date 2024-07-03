cb99adc6c94f46d3776881db8a6fb4c3b8d046eb
Renuka0794 <renukas462@gmail.com>
2024-07-03 18:26:14 +0530

    added image remote pattern

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**.pahadibaserahomestay.netlify.app',
+          port: '',
+        },
+      ],
+    },
+  }
+  pahadibaserahomestay.netlify.app
