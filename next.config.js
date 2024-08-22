a796cf27229ebb47fa7b31cc644aac08091522ba
Renuka0794 <renukas462@gmail.com>
2024-08-22 10:57:41 +0530

    added image config to unoptimize it

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {}
 
 module.exports = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**.pahadibaserahomestay.netlify.app',
-          port: '',
-        },
-      ],
+      unoptimized: true, 
     },
   }
